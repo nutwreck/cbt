90ce335b9310402aad50c4bd11accd27cd33fd27
candra <candra@arcacorp.com>
2022-08-31 18:00:12 +0700

    cache header

--- application/views/website/user/_template/header.php
+++ application/views/website/user/_template/header.php
@@ -1,44 +1,45 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="author" content="Candra Aji Pamungkas | candraajipamungkas@gmail.com">
-    <meta name="description" content="Aplikasi ujian online &amp; ujian online berbasis komputer Zambert">
-    <link rel="shortcut icon" href="<?php echo config_item('_assets_general'); ?>favicon/favicon.ico">
-    <meta property="og:description" content="Aplikasi ujian online &amp; ujian online berbasis komputer Zambert">
-    <meta property="og:title" content="Aplikasi Ujian Online">
-    <meta name="twitter:description" content="Aplikasi ujian online &amp; ujian online berbasis komputer Zambert">
-    <meta name="twitter:title" content="Aplikasi Ujian Online">
 
-    <title><?=$title?></title>
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv='cache-control' content='no-cache'>
+  <meta http-equiv='expires' content='0'>
+  <meta http-equiv='pragma' content='no-cache'>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Candra Aji Pamungkas | candraajipamungkas@gmail.com">
+  <meta name="description" content="Aplikasi ujian online &amp; ujian online berbasis komputer Zambert">
+  <link rel="shortcut icon" href="<?php echo config_item('_assets_general'); ?>favicon/favicon.ico">
+  <meta property="og:description" content="Aplikasi ujian online &amp; ujian online berbasis komputer Zambert">
+  <meta property="og:title" content="Aplikasi Ujian Online">
+  <meta name="twitter:description" content="Aplikasi ujian online &amp; ujian online berbasis komputer Zambert">
+  <meta name="twitter:title" content="Aplikasi Ujian Online">
 
-    <noscript>
-          <meta http-equiv="Refresh" content="0;<?php echo base_url(); ?>javascript-not-available">
-    </noscript>
+  <title><?= $title ?></title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-    <!-- CSS Internal -->
-    <link href="<?php echo config_item('_assets_website'); ?>css/style.css" rel="stylesheet" type="text/css" />
+  <noscript>
+    <meta http-equiv="Refresh" content="0;<?php echo base_url(); ?>javascript-not-available">
+  </noscript>
 
-    <!-- FONT -->
-    <!-- <link href="https://fonts.cdnfonts.com/css/arial-rounded-mt-bold" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <!-- CSS Internal -->
+  <link href="<?php echo config_item('_assets_website'); ?>css/style.css" rel="stylesheet" type="text/css" />
+
+  <!-- FONT -->
+  <!-- <link href="https://fonts.cdnfonts.com/css/arial-rounded-mt-bold" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap" rel="stylesheet"> -->
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
 
-    <!-- Scrollbar Custom CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
-    
-    <!-- ICON -->
-    <link rel="stylesheet" href="<?php echo config_item('_assets_general'); ?>font-awesome/css/font-awesome.min.css">
+  <!-- Scrollbar Custom CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css">
 
-    <!-- POP UP -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet" />
-  
+  <!-- ICON -->
+  <link rel="stylesheet" href="<?php echo config_item('_assets_general'); ?>font-awesome/css/font-awesome.min.css">
 
-    
+  <!-- POP UP -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet" />
